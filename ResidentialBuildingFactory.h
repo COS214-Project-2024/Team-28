d42e5c9121d9fc1179b45e02c9f24836899708d5
AntiDope00 <150138622+AntiDope00@users.noreply.github.com>
2024-10-29 13:04:38 +0200

    Add files via upload

---
+++ ResidentialBuildingFactory.h
@@ -0,0 +1,39 @@
+// ResidentialBuildingFactory.h
+#ifndef RESIDENTIALBUILDINGFACTORY_H
+#define RESIDENTIALBUILDINGFACTORY_H
+
+#include "BuildingFactory.h"
+#include "ResidentialBuildings.h"
+
+class ResidentialBuildingFactory : public BuildingFactory {
+public:
+    ResidentialBuilding* createHouse() const override {
+        return new House();
+    }
+
+    ResidentialBuilding* createFlat() const override {
+        return new Flat();
+    }
+
+    ResidentialBuilding* createTownHouse() const override {
+        return new TownHouse();
+    }
+
+    ResidentialBuilding* createEstate() const override {
+        return new Estate();
+    }
+
+
+
+    
+
+    // Returns nullptr for other types as ResidentialBuildingFactory only creates residential buildings
+    CommercialBuilding* createShop() const override { return nullptr; }
+    CommercialBuilding* createOffice() const override { return nullptr; }
+    IndustrialBuilding* createFactory() const override { return nullptr; }
+    IndustrialBuilding* createWarehouse() const override { return nullptr; }
+    Landmark* createPark() const override { return nullptr; }
+    Landmark* createMonument() const override { return nullptr; }
+};
+
+#endif
