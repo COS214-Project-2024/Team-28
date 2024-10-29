2f5e2bd7a0f6394950ac901377906e4ad92d808a
MagundaTakudzwa <u22599160@tuks.co.za>
2024-10-29 22:18:14 +0200

    added plantmanager class

---
+++ PlantsManager.cpp
@@ -0,0 +1,28 @@
+#include <exception>
+using namespace std;
+
+#include "PlantsManager.h"
+#include "Plants.h"
+#include "PlantStateHandler.h"
+#include "CityManager.h"
+
+void PlantsManager::initiateFaultHandling(String aFaultType) {
+	throw "Not yet implemented";
+}
+
+void PlantsManager::restorePlantOperations() {
+	throw "Not yet implemented";
+}
+
+void PlantsManager::performRoutineMaintenance() {
+	throw "Not yet implemented";
+}
+
+void() PlantsManager::changeState(String aNewState) {
+	throw "Not yet implemented";
+}
+
+void PlantsManager::fixtPlant() {
+	throw "Not yet implemented";
+}
+
---
+++ PlantsManager.h
@@ -0,0 +1,27 @@
+#ifndef PlantsManager_h
+#define PlantsManager_h
+
+#include <string>    // Include the string header for std::string
+#include "CityManager.h"
+
+class Plants;
+class PlantStateHandler;
+
+class PlantsManager : public CityManager {
+private:
+    std::string state;
+    int capacity;
+    int operationHours;
+
+public:
+    // Plants* unnamed_Plants;
+    // PlantStateHandler* unnamed_PlantStateHandler;
+
+    void initiateFaultHandling(const std::string& faultType);
+    void restorePlantOperations();
+    void performRoutineMaintenance();
+    void changeState(const std::string& newState);
+    void fixPlant();
+};
+
+#endif  // PlantsManager_h
