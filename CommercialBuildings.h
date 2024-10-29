33806bdea74b177371417d9651b81ae5ce9fe1fa
AntiDope00 <150138622+AntiDope00@users.noreply.github.com>
2024-10-29 13:32:09 +0200

    Add files via upload

---
+++ CommercialBuildings.h
@@ -0,0 +1,162 @@
+#ifndef COMMERCIALBUILDINGS_H
+#define COMMERCIALBUILDINGS_H
+
+
+//Concrete Products of the Commerical Building Abstract Product 
+//Shops, offices, malls.
+
+
+#include <iostream>
+#include "Building.h"
+
+class Shop: public CommercialBuilding{
+    public:
+     Shop():CommercialBuilding(){
+            this->address = "";
+            this->shopName = "";
+            this->typeOfShop = "";
+            this->shopSize = 0;
+            this->buildingCost = 50;
+
+        }
+        Shop(std::string address, std::string shopName, std::string typeOfShop,int shopSize ){
+            this->address = address;
+            this->shopName = shopName;
+            this->typeOfShop = typeOfShop;
+            this->shopSize = shopSize;
+
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~Shop() = default;
+        int getSize(){
+            return this->shopSize;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"Shop Details: "<<std::endl;
+            std::cout<<"Name: "<<shopName<<std::endl;
+            std::cout<<"Type: "<<typeOfShop<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"Shop Size Category "<<shopSize<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+
+
+    private:
+        std::string address;
+        std::string shopName;
+        std::string typeOfShop;
+        int shopSize;           // we will take taxes depoending on howw big the sie is, example shopSize 1 shops will pay 10, shopSize 2 will pay 20 etc
+        float buildingCost;
+};
+
+class Office: public CommercialBuilding{
+    public:
+        Office():CommercialBuilding(){
+            this->address = "";
+            this->companyName = "";
+            this->typeOfCompany = "";
+            this->officeSize = 0;
+            this->buildingCost = 70;
+
+        }
+        Office(std::string address, std::string companyName, std::string typeOfCompany,int shopSize ){
+            this->address = address;
+            this->companyName = companyName;
+            this->typeOfCompany = typeOfCompany;
+            this-> officeSize = officeSize;
+
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~Office() = default;
+        int getSize(){
+            return this->officeSize;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"Office Details: "<<std::endl;
+            std::cout<<"Name: "<<companyName<<std::endl;
+            std::cout<<"Type: "<<typeOfCompany<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"Office Size Category "<<officeSize<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+
+
+    private:
+        std::string address;
+        std::string companyName;
+        std::string typeOfCompany;
+        int officeSize;           // we will take taxes depoending on howw big the sie is, example shopSize 1 shops will pay 10, shopSize 2 will pay 20 etc
+        float buildingCost;
+};
+
+
+class Mall: public CommercialBuilding{
+    public:
+     Mall():CommercialBuilding(){
+            this->address = "";
+            this->mallName = "";
+            this->numberOfStores = 0;
+            this->buildingCost = 50;
+
+        }
+        Mall(std::string address, std::string mallName,int numberOfStores ){
+            this->address = address;
+            this->mallName = mallName;
+            this->numberOfStores= numberOfStores;
+
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~Mall() = default;
+        int getSize(){
+            return this->numberOfStores;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"Mall Details: "<<std::endl;
+            std::cout<<"Mall Name: "<<mallName<<std::endl;
+            std::cout<<"Number of Stores: "<<numberOfStores<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+    /*
+        if we want mall to be made up of shops and offices
+         //getNumberOfUnits and buildCost will change 
+    */
+
+
+    private:
+        std::string address;
+        std::string mallName;
+        int numberOfStores;           // we will take taxes depoending on howw big the sie is, example shopSize 1 shops will pay 10, shopSize 2 will pay 20 etc
+        float buildingCost;
+       
+        //If we want a mall to be made up of diiferent shops and offices intead of stores
+        /*
+            Shop*  shops;
+            Office* offices;
+            */
+};
+
+
+
+#endif
---
+++ ResidentialBuildings.h
@@ -0,0 +1,210 @@
+#ifndef RESIDENTIALBUILDINGS_H
+#define RESIDENTIALBUILDINGS_H
+
+
+//Concrete Products of the Residential Building Abstract Product 
+//Houses, flats/apartments, townhouses, estates.
+
+
+#include <iostream>
+#include "Building.h"
+
+class House: public ResidentialBuilding{
+
+    public:
+        House():ResidentialBuilding(){
+            this->address = "";
+            this->numberOfResidents = 0;
+            this->buildingCost = 10;
+        }
+        House(std::string address, int numberOfResidents){
+            this->address = address;
+            this->numberOfResidents = numberOfResidents;
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~House() = default;
+        int getNumberOfResidents(){      //Get numberOfResidents for citizen count 
+            return this->numberOfResidents;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"House Details: "<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"Number Of Residents: "<<numberOfResidents<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+    private:
+        std::string address;               //address
+        int numberOfResidents;                         //number of residents in house 
+        float buildingCost;          //cost to build the house defult is 10 
+    
+    
+
+
+
+};
+
+class Flat:public ResidentialBuilding{
+     public:
+        Flat():ResidentialBuilding(){
+            this->address = "";
+            this->numberOfResidents = 0;
+            this->buildingCost = 100;
+        }
+        Flat(std::string address, int numberOfResidents){
+            this->address = address;
+            this->numberOfResidents = numberOfResidents;
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~Flat() = default;
+        int getnumberOfResidents(){      //Get numberOfResidents for citizen count 
+            return this->numberOfResidents;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"Flat Details: "<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"Number Of Residents: "<<numberOfResidents<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+    private:
+        std::string address;               //address
+        int numberOfResidents;                         //number of residents in house 
+        float buildingCost;         //cost to build the house defult is 10 
+
+
+
+};
+
+
+class TownHouse: public ResidentialBuilding{
+     public:
+        TownHouse():ResidentialBuilding(){
+            this->address = "";
+            this->numberOfResidents = 0;
+            this->buildingCost = 110;
+        }
+        TownHouse(std::string address, int numberOfResidents){
+            this->address = address;
+            this->numberOfResidents = numberOfResidents;
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~TownHouse() = default;
+        int getnumberOfResidents(){      //Get numberOfResidents for citizen count 
+            return this->numberOfResidents;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"Townhouse Details: "<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"Number Of Residents: "<<numberOfResidents<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+    private:
+        std::string address;               //address
+        int numberOfResidents;             //number of residents in house 
+        float buildingCost;        //cost to build the house defult is 10 
+
+
+
+
+
+};
+
+
+class Estate: public ResidentialBuilding{
+    public:
+        Estate():ResidentialBuilding(){
+            this->address = "";
+            this->numberOfUnits = 0;
+            this->numberOfResidents = 0;
+            this->buildingCost = 1000;
+            
+        }
+        Estate(std::string address, int numberOfUnits, int numberOfResidents){
+            this->address = address;
+            this->numberOfUnits = numberOfUnits;
+            this->numberOfResidents = numberOfResidents;
+        }
+        float getBuildCost(){
+            return this->buildingCost;          // return building cost for comparisons 
+        }
+        ~Estate() = default;
+        
+        int getNumberOfResidents(){      //Get numberOfResidents for citizen count 
+            return this->numberOfResidents;
+        }
+        int avergeResidentsPerUnit(){
+            this->avgPerUnit = this->numberOfUnits/this->numberOfUnits;
+            return this->avgPerUnit;
+        }
+        std::string getAddress(){   //We dont want same adresses 
+            return this->address;
+        }
+
+        void print() const override{
+            std::cout<<"----------"<<std::endl;
+            std::cout<<"Estate Details: "<<std::endl;
+            std::cout<<"Address: "<<address<<std::endl;
+            std::cout<<"Number of Units: "<<numberOfUnits<<std::endl;
+            std::cout<<"Number Of Residents: "<<numberOfResidents<<std::endl;
+            std::cout<<"Avergae Number Of Residents per unit: "<<avgPerUnit<<std::endl;
+            std::cout<<"----------"<<std::endl;
+        }
+        //fucntions should change if we want estate to be made up of houses and flats 
+        //getNumbherOfResidenst and build cost will change 
+    private:
+        std::string address;               //address
+        int numberOfUnits;                 //number of units in Estate
+        int numberOfResidents;  
+        int avgPerUnit;                       //number of residents in estate
+        float buildingCost;         //cost to build the house defult is 10 
+
+        //if we want an estate to be made up of other residential buildings instead of units
+        /*  House* houses;
+            Flats* flats;
+            TownHouses* thouses;
+            */
+
+
+
+
+};
+
+/*class ResidentialBuilding: public BuildingFactory{//Concrete Factory 
+    public:
+        ResidentialBuilding() : BuildingFactory(){}
+        int updatenumberOfResidents(int newnumberOfResidents) const override{
+            return newnumberOfResidents;
+        }
+        Building* createBuilding() const override{
+            return new House();     //How to add it to return a new flat??
+
+        }
+
+
+        
+
+};
+*/
+
+#endif
+
