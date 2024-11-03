2970a33a02257636f28cb3cc3f054bcfd294b117
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:53:26 +0200

    Update Running.h

--- Running.h
+++ Running.h
@@ -3,6 +3,15 @@
 
 #include "PowerPlantState.h"
 
-class Running : public PowerPlantState
-{};
-#endif
+/**
+ * @class Running
+ * @brief Represents the running state of a power plant.
+ *
+ * This class inherits from PowerPlantState and signifies that the power
+ * plant is currently operational and generating energy.
+ */
+class Running : public PowerPlantState {
+    // Currently, no additional members or methods are defined.
+};
+
+#endif // RUNNING_H
