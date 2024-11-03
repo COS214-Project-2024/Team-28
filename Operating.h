51cdd614a70099ccb8a23e668ddf8fc0ac21cccc
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:39:49 +0200

    Update Operating.h

--- Operating.h
+++ Operating.h
@@ -1,7 +1,19 @@
 #ifndef OPERATING_H
 #define OPERATING_H
+
 #include "WastePlantState.h"
 
+/**
+ * @class Operating
+ * @brief Class representing the operating state of the waste plant.
+ *
+ * This class inherits from the WastePlantState interface, representing the state 
+ * when the waste plant is actively processing waste. It may implement specific
+ * behaviors or methods related to the operational phase of the plant.
+ */
 class Operating : public WastePlantState
-{};
-#endif
+{
+    // Future functionality specific to the Operating state can be added here.
+};
+
+#endif // OPERATING_H
