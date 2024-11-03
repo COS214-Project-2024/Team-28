ca21456696aadd1da8a957d300898b18f689914e
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:32:40 +0200

    Update Dependent.h

--- Dependent.h
+++ Dependent.h
@@ -1,10 +1,25 @@
+// Dependent.h
 #ifndef DEPENDENT_H
 #define DEPENDENT_H
 
 #include "Citizen.h"
 
+/**
+ * @class Dependent
+ * @brief Represents a dependent citizen.
+ *
+ * The Dependent class inherits from the Citizen class and
+ * represents a citizen who is dependent, potentially lacking
+ * certain responsibilities like tax payment or housing ownership.
+ */
 class Dependent : public Citizen {
 public:
+    /**
+     * @brief Constructs a new Dependent object.
+     *
+     * @param government A pointer to the Government instance this dependent interacts with.
+     * @param isHouseOwner A boolean indicating if the dependent is a house owner.
+     */
     Dependent(Government* government, bool isHouseOwner);
 };
 
