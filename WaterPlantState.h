eb8d8b3cdd94515b3eb030dbf055632d91815740
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:57:48 +0200

    Update WaterPlantState.h

--- WaterPlantState.h
+++ WaterPlantState.h
@@ -1,6 +1,15 @@
 #ifndef WATERPLANTSTATE_H
 #define WATERPLANTSTATE_H
 
-class WaterPlantState
-{};
-#endif
+/**
+ * @class WaterPlantState
+ * @brief Abstract base class representing the state of a water treatment plant.
+ *
+ * This class serves as the foundation for all specific states of the water treatment 
+ * plant, defining the common interface and behaviors for different plant states.
+ */
+class WaterPlantState {
+    // This class can be extended with additional data members and methods as needed
+};
+
+#endif // WATERPLANTSTATE_H
