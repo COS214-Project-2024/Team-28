fb6ce32710add1eaaf18213223f6cc8ef7adac7a
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:32:27 +0200

    Update Contaminated.h

--- Contaminated.h
+++ Contaminated.h
@@ -1,7 +1,21 @@
+// Contaminated.h
 #ifndef CONTAMINATED_H
 #define CONTAMINATED_H
+
 #include "WaterPlantState.h"
 
-class Contaminated : public WaterPlantState
-{};
-#endif
+/**
+ * @class Contaminated
+ * @brief Represents the contaminated state of a water plant.
+ *
+ * The Contaminated class inherits from WaterPlantState and
+ * represents a state where the water plant is contaminated.
+ */
+class Contaminated : public WaterPlantState {
+    // This class can be expanded with methods and properties
+    // specific to the contaminated state in the future.
+};
+
+#endif // CONTAMINATED_H
+
+
