fb826e601ae5d7c4f1e0e3b8d44c069e15a2ce53
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:42:11 +0200

    Update OperationalSate.h

--- OperationalSate.h
+++ OperationalSate.h
@@ -1,8 +1,20 @@
 #ifndef OPERATIONALSTATE_H
 #define OPERATIONALSTATE_H
+
 #include "SewagePlantState.h"
 
+/**
+ * @class OperationalState
+ * @brief Represents the operational state of the sewage plant.
+ *
+ * This class inherits from the SewagePlantState interface and encapsulates
+ * the behavior and properties relevant to when the sewage plant is 
+ * functioning normally.
+ */
 class OperationalState : public SewagePlantState
 {
+public:
+    // Additional methods and attributes can be added here as needed.
 };
-#endif
+
+#endif // OPERATIONALSTATE_H
