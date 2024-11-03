19479d634045074b973d5848d8d075e38fb5eb6b
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:57:29 +0200

    Update WastePlantState.h

--- WastePlantState.h
+++ WastePlantState.h
@@ -1,24 +1,33 @@
 #ifndef WASTEPLANTSTATE_H
 #define WASTEPLANTSTATE_H
 
-class WastePlantState
-{
+/**
+ * @class WastePlantState
+ * @brief Abstract base class representing the state of a waste management plant.
+ *
+ * This class serves as the base for all specific states of the waste management 
+ * plant, encapsulating the common interface and behaviors for different plant states.
+ */
+class WastePlantState {
 private:
+    /* Private data members can be added here */
 
-    /* data */
 public:
+    /**
+     * @brief Default constructor for WastePlantState.
+     *
+     * This constructor initializes the state of the waste plant.
+     * 
+     * @param args Optional parameters for initialization (if any).
+     */
     WastePlantState(/* args */);
-    ~WastePlantState();
+
+    /**
+     * @brief Destructor for WastePlantState.
+     *
+     * Cleans up any resources held by the WastePlantState.
+     */
+    virtual ~WastePlantState();
 };
-#endif
 
-
-// WastePlantState::WastePlantState(/* args */)
-// {
-// }
-
-
-// WastePlantState::~WastePlantState()
-// {
-// }
-
+#endif // WASTEPLANTSTATE_H
