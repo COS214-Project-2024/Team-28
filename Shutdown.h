e2a2abcaf9e334b9d5794da676f6215435df3754
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:54:48 +0200

    Update Shutdown.h

--- Shutdown.h
+++ Shutdown.h
@@ -1,8 +1,18 @@
 #ifndef SHUTDOWN_H
 #define SHUTDOWN_H
+
 #include "PowerPlantState.h"
 
-class Shutdown : public PowerPlantState
-{
+/**
+ * @class Shutdown
+ * @brief Represents the shutdown state of a power plant.
+ *
+ * This class derives from PowerPlantState and signifies that the power
+ * plant is in a shutdown state. Specific behavior and attributes related 
+ * to this state can be implemented in this class.
+ */
+class Shutdown : public PowerPlantState {
+    // Future implementation can include attributes and methods specific to the shutdown state
 };
-#endif
+
+#endif // SHUTDOWN_H
