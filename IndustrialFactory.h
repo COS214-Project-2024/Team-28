fb59e88ab61910b0c7aae81257df3838cec14b5e
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:36:45 +0200

    Update IndustrialFactory.h

--- IndustrialFactory.h
+++ IndustrialFactory.h
@@ -7,16 +7,47 @@
 #include "Warehouses.h"
 
 // Industrial Factory
+
+/**
+ * @class IndustrialFactory
+ * @brief A factory for creating industrial buildings.
+ *
+ * The IndustrialFactory class implements the BuildingFactory interface,
+ * providing concrete methods to create different types of industrial 
+ * buildings such as factories and warehouses.
+ */
 class IndustrialFactory : public BuildingFactory {
 public:
+    /**
+     * @brief Creates a new factory instance.
+     *
+     * This method returns a pointer to a newly created Factories object.
+     * It creates a factory with a new SteelBuilder as its material hub.
+     *
+     * @return A pointer to the created IndustrialBuilding (Factories).
+     */
     IndustrialBuilding* createFactory() const override {
         return new Factories(new SteelBuilder());
     }
 
+    /**
+     * @brief Creates a new warehouse instance.
+     *
+     * This method returns a pointer to a newly created Warehouses object.
+     *
+     * @return A pointer to the created IndustrialBuilding (Warehouses).
+     */
     IndustrialBuilding* createWarehouse() const override {
         return new Warehouses();
     }
 
+    /**
+     * @brief Creates a new plant instance.
+     *
+     * This method currently returns a nullptr as the plant creation is not implemented.
+     *
+     * @return A pointer to an IndustrialBuilding (nullptr).
+     */
     IndustrialBuilding* createPlant() const override {
         return nullptr;
     }
