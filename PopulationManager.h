23277d5249d880e8968a5b858cdc311d3d966f85
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:47:18 +0200

    Update PopulationManager.h

--- PopulationManager.h
+++ PopulationManager.h
@@ -1,32 +1,57 @@
-#include <exception>
-using namespace std;
-
 #ifndef __PopulationManager_h__
 #define __PopulationManager_h__
 
+#include <exception>
 #include "Citizen.h"
-// #include "HealthServices.h"
-// #include "CityManager.h"
 
-class Citizen;
+// Forward declarations
 class HealthServices;
 class CityManager;
-class PopulationManager;
 
-class PopulationManager
-{
-	private: List<Citizen> _citizens;
-	private: int _count;
-	public: HealthServices* _unnamed_HealthServices_;
-	public: CityManager* _unnamed_CityManager_;
+/**
+ * @class PopulationManager
+ * @brief Manages the population within the city.
+ *
+ * The PopulationManager class is responsible for tracking the citizens,
+ * managing population growth, and evaluating the employment status of 
+ * the population. It interacts with health services and city management.
+ */
+class PopulationManager {
+private:
+    List<Citizen> _citizens; ///< List of citizens in the population manager.
+    int _count;              ///< Total count of citizens.
 
-	public: void addCitizen(Citizen aCitizen);
+public:
+    HealthServices* _unnamed_HealthServices_; ///< Pointer to health services.
+    CityManager* _unnamed_CityManager_;       ///< Pointer to city manager.
 
-	public: void removeCitizen(Citizen aCitizen);
+    /**
+     * @brief Adds a citizen to the population manager.
+     * @param aCitizen The citizen to be added.
+     */
+    void addCitizen(Citizen aCitizen);
 
-	public: void trackPopulationGrowth();
+    /**
+     * @brief Removes a citizen from the population manager.
+     * @param aCitizen The citizen to be removed.
+     */
+    void removeCitizen(Citizen aCitizen);
 
-	public: void evaluateEmploymentStatus();
+    /**
+     * @brief Tracks the growth of the population.
+     *
+     * This method analyzes the current population and updates statistics 
+     * related to population growth.
+     */
+    void trackPopulationGrowth();
+
+    /**
+     * @brief Evaluates the employment status of the population.
+     *
+     * This method checks the employment situation of the citizens 
+     * and makes recommendations or changes if necessary.
+     */
+    void evaluateEmploymentStatus();
 };
 
-#endif
+#endif // __PopulationManager_h__
