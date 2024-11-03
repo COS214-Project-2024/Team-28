abc572bdf96ed62c0b5ccbdce30718b692625797
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:42:50 +0200

    Update PartialState.h

--- PartialState.h
+++ PartialState.h
@@ -1,8 +1,19 @@
 #ifndef PARTIALSTATE_H
 #define PARTIALSTATE_H
+
 #include "WastePlantState.h"
 
+/**
+ * @class PartialState
+ * @brief Represents the partial operational state of the waste treatment plant.
+ *
+ * This class inherits from the WastePlantState interface and defines
+ * the behavior and characteristics of the plant when it is only partially operational.
+ */
 class PartialState : public WastePlantState
 {
+public:
+    // Additional methods and attributes can be defined here as needed.
 };
-#endif
+
+#endif // PARTIALSTATE_H
