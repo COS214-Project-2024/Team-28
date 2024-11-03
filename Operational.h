e483c472180522726cb3fe1f11837b80d06b2fa6
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:40:53 +0200

    Update Operational.h

--- Operational.h
+++ Operational.h
@@ -1,20 +1,31 @@
 #ifndef OPERATIONAL_H
 #define OPERATIONAL_H
+
 #include "PowerPlantState.h"
 
+/**
+ * @class Operational
+ * @brief Class representing the operational state of the power plant.
+ *
+ * This class inherits from the PowerPlantState interface, representing the state 
+ * when the power plant is functioning normally. It may include methods and 
+ * properties relevant to the operational status of the plant.
+ */
 class Operational : public PowerPlantState
 {
 private:
-    /* data */
+    // Private data members related to the operational state can be added here.
+
 public:
-    Operational(/* args */);
+    /**
+     * @brief Default constructor for the Operational class.
+     */
+    Operational();
+
+    /**
+     * @brief Destructor for the Operational class.
+     */
     ~Operational();
 };
-#endif
-Operational::Operational(/* args */)
-{
-}
 
-Operational::~Operational()
-{
-}
+#endif // OPERATIONAL_H
