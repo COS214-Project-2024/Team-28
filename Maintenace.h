3c6bb768dbb13992c0ea1664a4ce6e5c71e4c041
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:38:05 +0200

    Update Maintenace.h

--- Maintenace.h
+++ Maintenace.h
@@ -1,8 +1,19 @@
 #ifndef MAINTENANCE_H
 #define MAINTENANCE_H
+
 #include "SewagePlantState.h"
 
-class Maintenance: public SewagePlantState
+/**
+ * @class Maintenance
+ * @brief Represents the maintenance state of a sewage treatment plant.
+ *
+ * The Maintenance class is derived from the SewagePlantState base class. It represents
+ * the state of the sewage treatment plant when it is undergoing maintenance operations.
+ * In this state, the plant may not be operational for regular sewage treatment processes.
+ */
+class Maintenance : public SewagePlantState
 {
+    // Additional member functions and attributes for the Maintenance state can be added here
 };
-#endif
+
+#endif // MAINTENANCE_H
