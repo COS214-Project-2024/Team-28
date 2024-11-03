02d8dbddb2febb7e607f7044aba931d32b86881f
Tumisho Makhene <150138622+AntiDope00@users.noreply.github.com>
2024-11-03 11:56:34 +0200

    Update Treatment.h

--- Treatment.h
+++ Treatment.h
@@ -1,8 +1,17 @@
-#ifndef TRATMENT_H
-#define TRATMENT_H
+#ifndef TREATMENT_H
+#define TREATMENT_H
+
 #include "WaterPlantState.h"
 
-class Treatment : public WaterPlantState
-{
+/**
+ * @class Treatment
+ * @brief A class representing the treatment state of a water plant.
+ *
+ * This class inherits from WaterPlantState and defines the behavior
+ * specific to the treatment phase of water processing.
+ */
+class Treatment : public WaterPlantState {
+    // Additional methods and member variables can be defined here.
 };
-#endif
+
+#endif // TREATMENT_H
