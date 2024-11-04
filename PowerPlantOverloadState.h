21d04f61495f978ae711e2b1d077a99626628f6f
Kuda21 <kudakwashemujuru1@gmail.com>
2024-11-04 16:06:18 +0200

    small changes

--- PowerPlantOverloadState.h
+++ PowerPlantOverloadState.h
@@ -6,14 +6,15 @@ using namespace std;
 
 class PowerPlantOverloadState : public PowerPlantState
 {
-    private:
-        int overloadThreshold;
-    public:
-        PowerPlantOverloadState(int threshold);
-        //void handlePowerState() override;
-        void restoreNormalOperations();
-        void update();
-        void handleState(Plant* plant) override;
-    std::string getStateName()  override ;
+private:
+    int overloadThreshold;
+
+public:
+    PowerPlantOverloadState(int threshold);
+    // void handlePowerState() override;
+    void restoreNormalOperations();
+    void update();
+    void handleState(Plant *plant) override;
+    std::string getStateName() override;
 };
 #endif
